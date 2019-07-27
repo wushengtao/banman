ac9f488bc70590e9488dc908d6c01e64e8344d12
lunzi <lunzi@maihaoche.com>
2019-07-27 11:29:41 +0800

    init project

---
+++ batman-web/src/main/java/com/lunzi/batman/web/BatmanWebApplication.java
@@ -0,0 +1,13 @@
+package com.lunzi.batman.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatmanWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatmanWebApplication.class, args);
+    }
+
+}
